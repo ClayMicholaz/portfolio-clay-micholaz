969583bb822bc781ae2b99803fd71abcf740835a
claymicholaz <clay.micholaz@profescipta.co.id>
2026-02-03 22:27:51 +0700

    feat: snowfall

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,28 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import { useSmoothScroll } from "./hooks/useSmoothScroll";
+import Snowfall from "react-snowfall";
 
 function App() {
   useSmoothScroll();
 
   return (
     <>
+      <Snowfall
+        color="#D1D5DB"
+        snowflakeCount={140}
+        speed={[0.4, 1.2]}
+        radius={[1, 3]}
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          pointerEvents: "none",
+          zIndex: 20,
+        }}
+      />
       <Navbar />
       <div
         id="scroll-container"
